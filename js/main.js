5f0db480c8a242cf6420091e776d8238c812a663
dodong <qkrdkwl9090@naver.com>
2021-04-19 22:17:24 +0900

    add Footer

--- js/main.js
+++ js/main.js
@@ -113,3 +113,8 @@ spyEls.forEach(function (spyEl){
         .setClassToggle(spyEl, 'show')
         .addTo(new ScrollMagic.Controller());
 });
+
+// FOOTER
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent=new Date().getFullYear();
